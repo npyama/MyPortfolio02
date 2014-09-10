1cda809a92f168b3efaccc2dba3fdd004fb1f20f
西山 大輔 <dnishiyama@crooz.co.jp>
2014-09-10 20:49:32 +0900

    コード綺麗にした

--- develop/scripts/main.js
+++ develop/scripts/main.js
@@ -1,58 +1,37 @@
-
-//Contents toggle
-/*
-$(function(){
-    $("#btnAboutMe").hover(
-    //マウスオーバー時の処理
-    function(){
-        $("#contentsAboutMe").css("display","block")
-        $("#contentsAboutMe").css("height","840px")
-    },
-    //マウスアウト時の処理
-    function(){
-        $("#contentsAboutMe").css("display","none")
-        $("#contentsAboutMe").css("height","0px")
-    });
-});
-*/
-
+/****************************************
+ Contents Toggle
+****************************************/
 $(function(){
  	$('#btnAboutMe').click(function() {
   		$('#contentsAboutMe').slideToggle(600, 'easeInQuint' , function() {
-    	// アニメーション完了後に実行したい処理
   		});
 	});
 });
-
 
 $(function(){
  	$('#btnSkill').click(function() {
   		$('#contentsSkill').slideToggle(600, 'easeInQuint' , function() {
-    	// アニメーション完了後に実行したい処理
   		});
 	});
 });
-
 
 $(function(){
  	$('#btnProduct').click(function() {
   		$('#contentsProduct').slideToggle(600, 'easeInQuint' , function() {
-    	// アニメーション完了後に実行したい処理
   		});
 	});
 });
+/******** END Contents Toggle ********/
+
 
 
 
 
 $(function(){
 
-
 /****************************************
 	hidden URL bar
 ****************************************/
-
-
 setTimeout(function(){  window.scrollTo(0,1);},1000); 
 $(function(){
 	window.scrollTo(0,1);
@@ -60,13 +39,9 @@ $(function(){
 
 
 
-
-
-
 /****************************************
 	タップ判定
 ****************************************/
-
 $('.contentsBtn').click(function() {
 	$(this).animate({ 
 	opacity: '0.7'
@@ -79,6 +54,18 @@ $('.contentsBtn').click(function() {
 
 
 
+/****************************************
+  least.js
+****************************************/
+$(document).ready(function(){
+    $('.least-gallery').least();
 });
+
+
+
+});
+
+
+
 
 
